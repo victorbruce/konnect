20924a6bbfc7bf73c9a0ee6bacbb2219c5498d23
victorbruce <victorbruce82@gmail.com>
2020-01-06 18:16:53 +0000

    Create restAPI to get and create screams

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,43 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccount.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://konnect-4088f.firebaseio.com"
 });
+
+const express = require('express');
+const app = express();
+
+// get screams
+app.get('/screams', (req, res) => {
+  admin.firestore().collection('screams').get()
+  .then(data => {
+    let screams = [];
+    data.forEach(doc => {
+      screams.push(doc.data())
+    });
+    return res.json(screams);
+  }).catch(error =>  console.error('Get screams', error))
+})
+
+// create scream
+app.post('/scream', (req, res) => {
+  let scream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  }
+
+  admin.firestore().collection('screams').add(scream)
+  .then(doc => {
+    return res.json({message: `document ${doc.id} created successfully`})
+  })
+  .catch(error => {
+    console.error('Create screams', error);
+    return res.status(500).json({error: 'something went wrong'});
+  })
+});
+
+exports.api = functions.https.onRequest(app);
