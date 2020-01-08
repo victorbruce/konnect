982daf9148d91c1aed5a5984a3678e1c64693fcb
victorbruce <victorbruce82@gmail.com>
2020-01-08 02:34:34 +0000

    add user login route

--- functions/index.js
+++ functions/index.js
@@ -143,6 +143,39 @@ app.post('/signup', (req, res) => {
     })
 });
 
+// login route
+app.post('/login', (req, res) => {
+  const user = {
+    email: req.body.email,
+    password: req.body.password
+  }
+  // validation
+  let errors = {};
 
+  if (isEmpty(user.email)) {
+    errors.email = 'Must not be empty';
+  }
+  if (isEmpty(user.password)) {
+    errors.password = 'Must not be empty';
+  }
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+  firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+    .then(data => {
+      return data.user.getIdToken();
+    })
+    .then(token => {
+      return res.json(token);
+    })
+    .catch(error => {
+      console.log(error);
+      if (error.code === 'auth/invalid-email') {
+        res.status(403).json({general: 'Wrong credentials, please try again'});
+      } else if (error.code === 'auth/user-not-found') {
+        res.status(403).json({general: 'Wrong credentials, please try again'});
+      }
+      return res.status(500).json({error: error.code});
+    })
+})
 
 exports.api = functions.https.onRequest(app);
