b034067a11bdf9d3ac3342422519a58be4f3f22a
victorbruce <victorbruce82@gmail.com>
2020-01-08 23:36:39 +0000

    add config module

---
+++ functions/utils/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyBavIsf9J0hWCwTTj3oz-tQLLu62093c7c",
+  authDomain: "konnect-4088f.firebaseapp.com",
+  databaseURL: "https://konnect-4088f.firebaseio.com",
+  projectId: "konnect-4088f",
+  storageBucket: "konnect-4088f.appspot.com",
+  messagingSenderId: "1003018423062",
+  appId: "1:1003018423062:web:3a4fe32e79d4bcd9cf5e37",
+  measurementId: "G-TJM2BX5P7H"
+}
