a302148fa916a53649661c6c262758438c3dee8a
victorbruce <victorbruce82@gmail.com>
2020-01-10 16:58:50 +0000

    Add getAuthenticatedUser and addUserDetails handlers

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,26 @@
-const functions = require('firebase-functions');
-const express = require('express');
+const functions = require("firebase-functions");
+const express = require("express");
 const app = express();
 
-const {getAllScreams, postOneScream} = require('./handlers/screams');
-const {signup, login, uploadImage} = require('./handlers/users');
-const FBAuth = require('./utils/fbAuth');
-
+const { getAllScreams, postOneScream } = require("./handlers/screams");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser
+} = require("./handlers/users");
+const FBAuth = require("./utils/fbAuth");
 
 // screams route
 
-app.get('/screams', getAllScreams)
-app.post('/scream', FBAuth, postOneScream);
+app.get("/screams", getAllScreams);
+app.post("/scream", FBAuth, postOneScream);
 
 // users route
-app.post('/signup', signup);
-app.post('/login', login);
-app.post('/user/image', FBAuth, uploadImage)
-
+app.post("/signup", signup);
+app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 exports.api = functions.https.onRequest(app);
