a47b0b89e6292ae44818e15b1a5c40c997c9ec0e
victorbruce <victorbruce82@gmail.com>
2020-01-10 13:46:55 +0000

    Fix image upload error

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,7 +1,10 @@
-const {admin, db} = require('../utils/admin');
-const firebase = require('../utils/config')
+const { admin, db } = require("../utils/admin");
+const firebase = require("../utils/config");
 
-const {validateSignupData, validateLoginData} = require('../utils/validators');
+const {
+  validateSignupData,
+  validateLoginData
+} = require("../utils/validators");
 
 exports.signup = (req, res) => {
   const newUser = {
@@ -9,23 +12,26 @@ exports.signup = (req, res) => {
     password: req.body.password,
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle
-  }
+  };
 
-  const {valid, errors } = validateSignupData(newUser);
+  const { valid, errors } = validateSignupData(newUser);
 
   if (!valid) return res.status(400).json(errors);
 
-  const noImg = 'no-img.png';
+  const noImg = "no-img.png";
 
   let token, userId;
-  db.doc(`/users/${newUser.handle}`).get()
+  db.doc(`/users/${newUser.handle}`)
+    .get()
     .then(doc => {
       // check if user handle already exists
       if (doc.exists) {
-        return res.status(400).json({ handle: 'this handle is already taken'})
+        return res.status(400).json({ handle: "this handle is already taken" });
       } else {
         // else create a new user
-        return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
       }
     })
     .then(data => {
@@ -40,33 +46,35 @@ exports.signup = (req, res) => {
         createdAt: new Date().toISOString(),
         imageUrl: `https://firebasestorage.googleapis.com/v0/b/konnect-4088f.appspot.com/o/${noImg}?alt=media`,
         userId
-      }
+      };
       // persist user to the database
-      return db.doc(`/users/${newUser.handle}`).set(userCredentials)
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
     })
     .then(() => {
-      res.status(201).json({token})
+      res.status(201).json({ token });
     })
     .catch(error => {
-      if (error.code === 'auth/email-already-in-use') {
-        return res.status(400).json({ email: 'Email is already in use'})
+      if (error.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use" });
       } else {
-        return res.status(500).json({error: error.code})
+        return res.status(500).json({ error: error.code });
       }
-    })
-}
+    });
+};
 
 exports.login = (req, res) => {
   const user = {
     email: req.body.email,
     password: req.body.password
-  }
-  
-  const {errors, valid} = validateLoginData(user);
+  };
+
+  const { errors, valid } = validateLoginData(user);
 
   if (!valid) return res.status(400).json(errors);
-  
-  firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
     .then(data => {
       return data.user.getIdToken();
     })
@@ -75,14 +83,18 @@ exports.login = (req, res) => {
     })
     .catch(error => {
       console.log(error);
-      if (error.code === 'auth/invalid-email') {
-        res.status(403).json({general: 'Wrong credentials, please try again'});
-      } else if (error.code === 'auth/user-not-found') {
-        res.status(403).json({general: 'Wrong credentials, please try again'});
+      if (error.code === "auth/invalid-email") {
+        res
+          .status(403)
+          .json({ general: "Wrong credentials, please try again" });
+      } else if (error.code === "auth/user-not-found") {
+        res
+          .status(403)
+          .json({ general: "Wrong credentials, please try again" });
       }
-      return res.status(500).json({error: error.code});
-    })
-}
+      return res.status(500).json({ error: error.code });
+    });
+};
 
 exports.uploadImage = (req, res) => {
   const BusBoy = require('busboy');
@@ -90,41 +102,49 @@ exports.uploadImage = (req, res) => {
   const os = require('os');
   const fs = require('fs');
 
-  const busboy = new BusBoy({headers: req.headers});
+  const busboy = new BusBoy({ headers: req.headers });
+
+  let imageToBeUploaded = {};
   let imageFileName;
-  let imageToBeUploaded;
-  
+
   busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-    // get file extension eg profile.png we want the 
-    // png to be stored in imageExtension
-    const imageExtension = filename.split('.')[filename.split('.').length - 1];
-    imageFileName = `${Math.round(Math.random)}.${imageExtension}`;
-    const filepath = path.join(os.tmpdir(), imageFileName);
-    imageToBeUploaded = {
-      filepath,
-      mimetype
+    console.log(fieldname, file, filename, encoding, mimetype);
+    if (mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
+      return res.status(400).json({ error: 'Wrong file type submitted' });
     }
+    // my.image.png => ['my', 'image', 'png']
+    const imageExtension = filename.split('.')[filename.split('.').length - 1];
+    // 32756238461724837.png
+    imageFileName = `${Math.round(
+      Math.random() * 1000000000000
+    ).toString()}.${imageExtension}`;
+    const filepath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filepath, mimetype };
     file.pipe(fs.createWriteStream(filepath));
   });
   busboy.on('finish', () => {
-    admin.storage().bucket().upload(imageToBeUploaded.filepath, {
-      resumable: false,
-      metadata: {
+    admin
+      .storage()
+      .bucket("konnect-4088f.appspot.com")
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
         metadata: {
-          contentType: imageToBeUploaded.mimetype
+          metadata: {
+            contentType: imageToBeUploaded.mimetype
+          }
         }
-      }
-    })
-    .then(() => {
-      const imageUrl = `https://firebasestorage.googleapis.com/v0/b/konnect-4088f.appspot.com/o/${imageFileName}?alt=media`;
-      return db.doc(`/users/${req.user.handle}`).update({ imageUrl })
-    })
-    .then(() => {
-      return res.json({message: 'Image uploaded successfully'})
-    })
-    .catch(error => {
-      return res.status(500).json({error: error.code})
-    })
+      })
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/konnect-4088f.appspot.com/o/${imageFileName}?alt=media`;
+        return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
+      })
+      .then(() => {
+        return res.json({ message: 'image uploaded successfully' });
+      })
+      .catch((error) => {
+        console.error(error);
+        return res.status(500).json({ error: 'something went wrong' });
+      });
   });
   busboy.end(req.rawBody);
-}
+};
--- functions/utils/admin.js
+++ functions/utils/admin.js
@@ -4,7 +4,8 @@ const firebase = require('./config');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://konnect-4088f.firebaseio.com"
+  databaseURL: "https://konnect-4088f.firebaseio.com",
+  storageBucket: "konnect-4088f.appspot.cokonnect-4088f.appspot.com"
 });
 
 
